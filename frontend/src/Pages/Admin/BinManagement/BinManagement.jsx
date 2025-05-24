f764caea3533a9d9be2488c87f12663cb438fc76
Nuhansi <146503953+UpenaNuhansi@users.noreply.github.com>
2025-05-24 11:15:37 +0530

    feat-added bin management page for enhanced admin user experience

---
+++ frontend/src/Pages/Admin/BinManagement/BinManagement.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+import { BarChart2, Bell, Settings, Users, Trash2, LogOut, User, Search, Edit, Wrench, MapPin, Wifi, WifiOff, Plus } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import TopBar from '../../../Components/TopBar/TopBar';
