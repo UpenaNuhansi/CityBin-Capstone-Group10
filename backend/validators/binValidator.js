6ee7c2615ad4a872757c6b98414e5b3d1afbf864
WEERASI <149503890+WEERASI@users.noreply.github.com>
2025-06-01 19:13:26 +0530

    binValidator.js

--- backend/validators/binValidator.js
+++ backend/validators/binValidator.js
@@ -1,1 +1,9 @@
+const { body } = require('express-validator');
+
+exports.binValidationRules = [
+  body('binId').notEmpty().withMessage('Bin ID is required'),
+  body('location').notEmpty().withMessage('Location is required'),
+  body('status').isIn(['empty', 'half-full', 'full']).withMessage('Invalid status'),
+  body('fillLevel').isNumeric().withMessage('Fill level must be a number'),
+];
 //bin validator.js
