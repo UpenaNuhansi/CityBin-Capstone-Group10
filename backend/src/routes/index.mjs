daf875fad68f5c4f9cefd09a3b376338af2b1e9a
omila11 <164768858+omila11@users.noreply.github.com>
2025-07-21 00:55:19 +0530

    Update index.mjs

--- backend/src/routes/index.mjs
+++ backend/src/routes/index.mjs
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./user.mjs";
+import binRouter from "./bin.mjs"; 
 
 const rootRouter = Router();
 
-rootRouter.use("/user",userRouter);
+rootRouter.use("/user", userRouter);
+rootRouter.use("/bin", binRouter); 
 
-export default rootRouter
+export default rootRouter;
