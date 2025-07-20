eb7783460ca387b557f527eb75b931f952c5ab55
omila11 <164768858+omila11@users.noreply.github.com>
2025-07-21 00:56:23 +0530

    Update server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,20 +1,29 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-
+import cors from "cors"; 
 import rootRouter from "./src/routes/index.mjs";
 
 const server = express();
 const PORT = process.env.PORT || 4000;
+ 
+server.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'self' http://localhost:5173; connect-src http://localhost:5001 http://localhost:5173; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';");
+  next();
+});
 
-// Middleware to parse JSON
+server.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true               
+}));
+
+
 server.use(express.json());
 
 
-//route connect
-server.use('api/v1',rootRouter)
+server.use('/api', rootRouter);
 
-// Connect to MongoDB
+
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log("db connected..");
@@ -24,5 +33,5 @@ mongoose.connect(process.env.MONGO_URL)
   })
   .catch((err) => {
     console.error("DB connection error:", err);
-    process.exit(1); // Exit process on DB failure
+    process.exit(1); 
   });
